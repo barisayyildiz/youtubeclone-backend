9ced479a15e13ab60c59594137942b3805d75045
Barış <barisayyildiz.1799@gmail.com>
2022-02-20 22:51:09 +0300

    cloudinary ye video yüklenebiliyor

--- routes/cloudinary.ts
+++ routes/cloudinary.ts
@@ -15,7 +15,7 @@ router.post("/image", upload.single('avatar'), (req:MulterRequest, res:Response)
 	console.log(req.file)
 	const { path, filename } = req.file
 
-	// upload image here
+	// upload image
 	cloudinary.uploader
 	.upload(path)
 	.then(async (result:any) => {
@@ -32,6 +32,30 @@ router.post("/image", upload.single('avatar'), (req:MulterRequest, res:Response)
 			err
 		})
 	})
+})
+
+router.post("/video", upload.single('video'), (req:MulterRequest, res:Response) => {
+	const { path, filename } = req.file
+	
+	// upload video
+	cloudinary.uploader.upload(path, {
+		resource_type: "video"
+	})
+	.then(async (result:any) => {
+		// remove file from disk
+		await fs.promises.unlink(`./uploads/${filename}`)
+		res.status(200).send({
+			msg:'success',
+			result
+		}) 
+	})
+	.catch((err:any) => {
+		res.status(500).send({
+			msg:'fail',
+			err
+		})
+	})
+
 })
 
 export default router
