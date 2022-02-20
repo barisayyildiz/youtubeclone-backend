f01d9681d6b1b634063f093bdcf3d1b5e70c216b
Barış <barisayyildiz.1799@gmail.com>
2022-02-20 20:55:22 +0300

    gereksiz dosyalar silindi

---
+++ config/cloudinary.ts
@@ -0,0 +1,16 @@
+require("dotenv")
+const cloudinary = require("cloudinary").v2
+
+const {
+	CLOUD_NAME,
+	CLOUD_API_KEY,
+	CLOUD_API_SECRET
+} = process.env
+
+cloudinary.config({
+	cloud_name : CLOUD_NAME,
+	api_key : CLOUD_API_KEY,
+	api_secret : CLOUD_API_SECRET
+})
+
+export default cloudinary
